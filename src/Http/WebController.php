acac7884804f368408e4cbe3b2c31cb404ff55f9
SDigitalOficial <144869467+SDigitalOficial@users.noreply.github.com>
2024-04-01 15:38:01 -0500

    Ajustes plantillas
    Ajustes plantillas

--- src/Http/WebController.php
+++ src/Http/WebController.php
@@ -124,7 +124,7 @@ foreach($select as $select){
 $plantillas = GrapeTemp::where('id','=',$select->template)->get();
 }
 }
-return view('Templates.index')->with('menu', $menu)->with('pagina', $pagina)->with('seo', $seo);
+return view('Templates.index')->with('menu', $menu)->with('pagina', $pagina)->with('seo', $seo)->with('plantillas', $plantillas);
 }
 
 
